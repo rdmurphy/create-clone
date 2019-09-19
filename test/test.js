fe3a2f65c9f945f96f8597ac79ff219774aef6c9
Ryan Murphy <rdmurphy@users.noreply.github.com>
2019-09-19 12:51:43 -0700

    Add gitlab and bitbucket to tests

--- test/test.js
+++ test/test.js
@@ -32,12 +32,54 @@ after(async () => {
   await fs.remove('.output');
 });
 
-describe('create-clone', () => {
-  it('should clone a repository from GitHub', async () => {
-    await createClone('rdmurphy/create-clone-test', '.output');
-    await compare(
-      '.output',
-      new Map([['index.txt', 'This is only a test.\n']])
-    );
+describe('create-clone + github', () => {
+  [
+    'rdmurphy/create-clone-test',
+    'github:rdmurphy/create-clone-test',
+    'https://github.com/rdmurphy/create-clone-test',
+    'git@github.com:rdmurphy/create-clone-test',
+  ].forEach(repo => {
+    it(repo, async () => {
+      await createClone(repo, '.output');
+
+      await compare(
+        '.output',
+        new Map([['index.txt', 'This is only a test.\n']])
+      );
+    });
+  });
+});
+
+describe('create-clone + gitlab', () => {
+  [
+    'gitlab:rdmurphy_/create-clone-test',
+    'https://gitlab.com/rdmurphy_/create-clone-test',
+    'git@gitlab.com:rdmurphy_/create-clone-test',
+  ].forEach(repo => {
+    it(repo, async () => {
+      await createClone(repo, '.output');
+
+      await compare(
+        '.output',
+        new Map([['index.txt', 'This is only a test.\n']])
+      );
+    });
+  });
+});
+
+describe('create-clone + bitbucket', () => {
+  [
+    'bitbucket:rdmurphy_/create-clone-test',
+    'https://bitbucket.org/rdmurphy_/create-clone-test',
+    'git@bitbucket.org:rdmurphy_/create-clone-test',
+  ].forEach(repo => {
+    it(repo, async () => {
+      await createClone(repo, '.output');
+
+      await compare(
+        '.output',
+        new Map([['index.txt', 'This is only a test.\n']])
+      );
+    });
   });
 });
